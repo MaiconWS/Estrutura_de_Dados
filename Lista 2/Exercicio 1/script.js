edba0722d382253d622d25adb1f9ccd0f33b5ca4
MaiconMW <maicon.w312@outlook.com>
2023-08-31 20:00:10 -0300

    Segunda lista de exercicio

---
+++ Lista 2/Exercicio 1/script.js
@@ -0,0 +1,15 @@
+let button = document.querySelector("button")
+
+button.addEventListener('click', () => {
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    let h2 = document.querySelector("h2");
+
+    if (num1 > num2)
+    h2.innerHTML = `${num1} é maior que o ${num2}`;
+    else
+        if (num2 > num1)
+        h2.innerHTML = `${num2} é maior que o ${num1}`;
+        else 
+        h2.innerHTML = `Os numeros são iguais`;
+})
